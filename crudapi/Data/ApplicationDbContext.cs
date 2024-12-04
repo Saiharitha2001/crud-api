ecb185b3d53c1def057e806b8138eb1f390874c2
Saiharitha2001 <saiharitha.vuthukadu@snaddevelopers.com>
2024-12-04 20:07:13 +0530

    Add project files.

---
+++ crudapi/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using crudapi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudapi.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Employee> Employees { get; set; }
+    }
+}
